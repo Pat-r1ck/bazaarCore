1813fb59fc8c8635d824fc2d32894a0ac7da1f92
patrick <ppusitdhikul@gmail.com>
2023-10-27 11:49:38 +0800

    feat(profile_service): implement user profile

---
+++ profile-service/src/main/java/com/okit/profileservice/constants/UserProfileCoreConstants.java
@@ -0,0 +1,10 @@
+package com.okit.profileservice.constants;
+
+public class UserProfileCoreConstants
+{
+    public static final String EMPTY_EMAIL_MSG = "Please provide email";
+    public static final String EMPTY_FIRST_NAME_MSG = "Please provide firstname";
+    public static final String EMPTY_LAST_NAME_MSG = "Please provide lastname";
+    public static final String INVALID_ACADEMIC_YEAR = "Academic year must be greater than 0";
+    public static final String INVALID_RATING_MSG = "Rating cannot be more than 5.";
+}
